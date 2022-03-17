fabff6e11863e1dc44833fb157b50566d1986e43
Lonely-Taco <47434636+Lonely-Taco@users.noreply.github.com>
2022-03-17 13:44:42 +0100

    adding book endpoints

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -2,9 +2,45 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UpdateBookRequest;
+use App\Models\Book;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
 
 class MovieController extends Controller
 {
-    //
+    public function index(): array|Collection
+    {
+        return Book::all();
+    }
+
+    public function show($id): Book
+    {
+        return Book::find($id);
+    }
+
+    public function create(UpdateBookRequest $request): Book
+    {
+        return Book::create($request->validated());
+    }
+
+    public function edit($id, UpdateBookRequest $request): Book
+    {
+        $game = Book::findOrFail($id);
+
+        $game->update($request->validated());
+
+        $game->save();
+
+        return $game;
+    }
+
+    public function destroy($id)
+    {
+        $game = Book::findOrFail($id);
+
+        $game->delete();
+
+        return response('', 204);
+    }
 }
