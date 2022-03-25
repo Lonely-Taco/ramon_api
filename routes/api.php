a40fa369c728b390428a276435fb7c82a44d9eef
Lonely-Taco <47434636+Lonely-Taco@users.noreply.github.com>
2022-03-25 13:57:36 +0100

    add documentation

--- routes/api.php
+++ routes/api.php
@@ -21,6 +21,10 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+//-----------------------------------------------------------------------------
+// Game endpoints
+//-----------------------------------------------------------------------------
+
 /**
  * @OA\Get(
  *     path="/api/games",
@@ -32,16 +36,16 @@ Route::get('/games', [GameController::class, 'index']);
 
 /**
  * @OA\Get   (
- *     path="/api/game/{id}",
- *     description="games",
+ *     path="/api/game/id={id}",
+ *     description="get a game",
  *     @OA\Response(response="default", description="get")
  * )
  */
-Route::get('/game/{id}', [GameController::class, 'show']);
+Route::get('/game/id={id}', [GameController::class, 'show']);
 
 /**
  * @OA\Patch  (
- *     path="/api/game/{id}",
+ *     path="/api/game/id={id}",
  *     description="games",
  *     @OA\Response(response="default", description="read")
  * )
@@ -50,7 +54,7 @@ Route::post('/game', [GameController::class, 'create']);
 
 /**
  * @OA\Post   (
- *     path="/api/game/{id}",
+ *     path="/api/game/?{id}",
  *     description="games",
  *     @OA\Response(response="default", description="post")
  * )
@@ -59,12 +63,16 @@ Route::patch('/game/id={id}', [GameController::class, 'edit']);
 
 /**
  * @OA\Delete (
- *     path="/api/game/{id}",
+ *     path="/api/game/id={id}",
  *     description="games",
  *     @OA\Response(response="default", description="delete")
  * )
  */
-Route::delete('/game/{id}', [GameController::class, 'destroy']);
+Route::delete('/game/id={id}', [GameController::class, 'destroy']);
+
+//-----------------------------------------------------------------------------
+// Book endpoints
+//-----------------------------------------------------------------------------
 
 /**
  * @OA\Get(
@@ -77,76 +85,85 @@ Route::get('/books', [BookController::class, 'index']);
 
 /**
  * @OA\Get   (
- *     path="/api/game/{id}",
- *     description="games",
+ *     path="/api/book/id={id}",
+ *     description="det books",
  *     @OA\Response(response="default", description="get")
  * )
  */
-Route::get('/book/{id}', [BookController::class, 'show']);
-
-/**
- * @OA\Patch  (
- *     path="/api/game/{id}",
- *     description="games",
- *     @OA\Response(response="default", description="read")
- * )
- */
-Route::post('/book', [BookController::class, 'create']);
+Route::get('/book/id={id}', [BookController::class, 'show']);
 
 /**
  * @OA\Post   (
- *     path="/api/game/{id}",
- *     description="games",
+ *     path="/api/book/",
+ *     description="create a book",
  *     @OA\Response(response="default", description="post")
  * )
  */
+Route::post('/book/', [BookController::class, 'create']);
+
+/**
+ * @OA\Patch  (
+ *     path="/api/book/id={id}",
+ *     description="update book",
+ *     @OA\Response(response="default", description="read")
+ * )
+ */
 Route::patch('/book/id={id}', [BookController::class, 'edit']);
+
 /**
  * @OA\Delete (
- *     path="/api/game/{id}",
+ *     path="/api/book/id={id}",
  *     description="games",
  *     @OA\Response(response="default", description="delete")
  * )
  */
-Route::delete('/book/{id}', [BookController::class, 'destroy']);
+Route::delete('/book/id={id}', [BookController::class, 'destroy']);
+
+//-----------------------------------------------------------------------------
+// Movie endpoints
+//-----------------------------------------------------------------------------
 
 /**
  * @OA\Get(
- *     path="/api/games",
- *     description="games",
- *     @OA\Response(response="default", description="Welcome page")
+ *     path="/api/movies",
+ *     description="movies",
+ *     @OA\Response(response="default", description="get all movies")
  * )
  */
 Route::get('/movies', [MovieController::class, 'index']);
+
 /**
  * @OA\Get   (
- *     path="/api/game/{id}",
- *     description="games",
+ *     path="/api/movie/id={id}",
+ *     description="get a movie",
  *     @OA\Response(response="default", description="get")
  * )
  */
-Route::get('/movie/{id}', [MovieController::class, 'show']);
-/**
- * @OA\Patch  (
- *     path="/api/game/{id}",
- *     description="games",
- *     @OA\Response(response="default", description="read")
- * )
- */
-Route::post('/movie', [MovieController::class, 'create']);
+Route::get('/movie/id={id}', [MovieController::class, 'show']);
+
 /**
  * @OA\Post   (
- *     path="/api/game/{id}",
+ *     path="/api/movie/",
+ *     description="create movie",
+ *     @OA\Response(response="default", description="create")
+ * )
+ */
+Route::post('/movie/id={id}', [MovieController::class, 'create']);
+
+/**
+ * @OA\Patch    (
+ *     path="/api/game/id={id}",
  *     description="games",
  *     @OA\Response(response="default", description="post")
  * )
  */
 Route::patch('/movie/id={id}', [MovieController::class, 'edit']);
+
 /**
  * @OA\Delete (
- *     path="/api/game/{id}",
- *     description="games",
+ *     path="/api/movie/id={id}",
+ *     description="delete movie",
  *     @OA\Response(response="default", description="delete")
  * )
  */
-Route::delete('/movie/{id}', [MovieController::class, 'destroy']);
+Route::delete('/movie/id={id}', [MovieController::class, 'destroy']);
